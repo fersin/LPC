9a065425814037567272f30faf11ff50d22606df
fersin <79020501+fersin@users.noreply.github.com>
2021-03-21 02:22:15 -0600

    Create bash.sh

---
+++ Tarea powershell bash/bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash                                                                                                                                                             myf () {
+        while :
+        do
+                echo "ingrese su edad "
+                read v
+                echo "hola su edad es $v"
+                if [[ $v -lt 18 ]]
+                then
+                        echo "usted no tiene edad para esto "
+                else
+                        echo "pasele :)"
+                fi
+                echo "desea continuar? 1 para si o presione CTRL+C "
+                read c
+                if [[ $c -eq 1 ]]
+                then
+                        continue
+                fi
+        done
+}
+myf $v
