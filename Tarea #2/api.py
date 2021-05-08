0e678286b54e52e228269dca1b1e342ccb2c71e4
fersin <79020501+fersin@users.noreply.github.com>
2021-05-08 00:43:24 -0500

    Update api.py

--- Tarea #2/api.py
+++ Tarea #2/api.py
@@ -1,10 +1,12 @@
 import requests
 
+#api #3
 ip=input("ingrese la ip que desea en formato ipv4")
 r=requests.get('http://api.ipapi.com/api/'+ip+'?access_key=b7b979141c657937ac4b05df225c0ca9')
 
 print(r.text)
 
+#api #2
 a=input("ingrese la ciudad que quiera ")
 
 api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=7cb793fd1e6bc19d787d866b4374a320&q='
